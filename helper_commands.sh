4acf97ab16427211234d3a9984de91b2dde73652
Alecs Popa <alecs.popa@gmail.com>
2017-05-24 12:32:24 +0300

    Cleanup helper commands

--- helper_commands.sh
+++ helper_commands.sh
@@ -7,65 +7,27 @@ hdfs dfs -copyFromLocal /tmp/twitter_mention.json /user/hadoopuser/input/twitter
 hdfs dfs -copyFromLocal /tmp/twitter_mention_100k.json /user/hadoopuser/input/twitter_mention_100k.json
 hdfs dfs -copyFromLocal /tmp/twitter_mention_250k.json /user/hadoopuser/input/twitter_mention_250k.json
 hdfs dfs -copyFromLocal /tmp/twitter_mention_500k.json /user/hadoopuser/input/twitter_mention_500k.json
+hdfs dfs -copyFromLocal /tmp/twitter_mention_750k.json /user/hadoopuser/input/twitter_mention_750k.json
 
 hdfs dfs -mkdir -p /user/hadoopuser/input
 hdfs dfs -mkdir -p /user/hadoopuser/output
-
-hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-    org.apache.giraph.GiraphRunner -D \
-    giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181" \
-    org.school.rumorspread.RumorSpreadComputation \
-    -vif org.school.rumorspread.RumorSpreadInputFormat \
-    -vip /user/hadoopuser/input/twitter_mention.json \
-    -vof org.school.rumorspread.RumorSpreadOutputFormat \
-    -w 3 \
-    -op /user/hadoopuser/output/rumorspread-full-dataset
-
-hdfs dfs -copyFromLocal /tmp/HEPT_edge_list.json /user/hadoopuser/input/HEPT_edge_list.json
-
-hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-    org.apache.giraph.GiraphRunner -D \
-    giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181" \
-    org.school.rumorspread.RumorSpreadComputation \
-    -vif org.school.rumorspread.RumorSpreadInputFormat \
-    -vip /user/hadoopuser/input/HEPT_edge_list.json \
-    -vof org.school.rumorspread.RumorSpreadOutputFormat \
-    -w 1 \
-    -op /user/hadoopuser/output/hept-full-dataset
-
-hdfs dfs -copyFromLocal /tmp/twitter_mention_50k.json /user/hadoopuser/input/twitter_mention_50k.json
 
 # --------
 
 hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
     org.school.rumorspread.GiraphAppRunner \
     -in /user/hadoopuser/input/HEPT_edge_list.json \
-    -out /user/hadoopuser/output/hept-full-dataset \
-    -w 1 \
-    giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181" \
+    -out /user/hadoopuser/output/HEPT_edge_list \
+    -w 2 \
+    giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181,datanode3:2181,datanode4:2181,datanode5:2181,datanode6:2181,datanode7:2181" \
     org.school.rumorspread.RumorSpreadComputation
+
 
 hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
     org.school.rumorspread.GiraphAppRunner \
-    -in /user/hadoopuser/input/HEPT_edge_list_75_percent.json \
-    -out /user/hadoopuser/output/hept-75-percent-dataset \
-    -w 1 \
-    giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181" \
-    org.school.rumorspread.RumorSpreadComputation
-
-hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-    org.school.rumorspread.GiraphAppRunner \
-    -in /user/hadoopuser/input/twitter_mention_50k.json \
-    -out /user/hadoopuser/output/twitter-mention-50k \
-    -w 1 \
-    giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181" \
-    org.school.rumorspread.RumorSpreadComputation
-
-hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
-    org.school.rumorspread.GiraphAppRunner \
-    -in /user/hadoopuser/input/twitter_mention_250k.json \
-    -out /user/hadoopuser/output/twitter-mention-250k \
-    -w 8 \
+    -in /user/hadoopuser/input/twitter_mention_500k.json \
+    -out /user/hadoopuser/output/twitter-mention_500k \
+    -w 4 \
     giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181,datanode3:2181,datanode4:2181,datanode5:2181,datanode6:2181,datanode7:2181" \
     org.school.rumorspread.RumorSpreadComputation
 
