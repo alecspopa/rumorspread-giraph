32b79085d93a490fed44fbe65fa8e56fb10d12ac
Alecs Popa <alecs.popa@gmail.com>
2017-06-02 17:22:59 +0300

    Update helper commands

--- helper_commands.sh
+++ helper_commands.sh
@@ -5,9 +5,8 @@ cp /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar /home/ha
 
 hdfs dfs -copyFromLocal /tmp/twitter_mention.json /user/hadoopuser/input/twitter_mention.json
 hdfs dfs -copyFromLocal /tmp/twitter_mention_100k.json /user/hadoopuser/input/twitter_mention_100k.json
-hdfs dfs -copyFromLocal /tmp/twitter_mention_250k.json /user/hadoopuser/input/twitter_mention_250k.json
-hdfs dfs -copyFromLocal /tmp/twitter_mention_500k.json /user/hadoopuser/input/twitter_mention_500k.json
-hdfs dfs -copyFromLocal /tmp/twitter_mention_750k.json /user/hadoopuser/input/twitter_mention_750k.json
+hdfs dfs -copyFromLocal /tmp/twitter_mention_200k.json /user/hadoopuser/input/twitter_mention_200k.json
+hdfs dfs -copyFromLocal /tmp/twitter_mention_400k.json /user/hadoopuser/input/twitter_mention_400k.json
 
 hdfs dfs -mkdir -p /user/hadoopuser/input
 hdfs dfs -mkdir -p /user/hadoopuser/output
@@ -25,8 +24,8 @@ hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
 
 hadoop jar /tmp/org.school.rumorspread-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
     org.school.rumorspread.GiraphAppRunner \
-    -in /user/hadoopuser/input/twitter_mention_500k.json \
-    -out /user/hadoopuser/output/twitter-mention_500k \
+    -in /user/hadoopuser/input/twitter_mention_200k.json \
+    -out /user/hadoopuser/output/twitter-mention_200k \
     -w 4 \
     giraph.zkList="namenode:2181,datanode:2181,datanode1:2181,datanode2:2181,datanode3:2181,datanode4:2181,datanode5:2181,datanode6:2181,datanode7:2181" \
     org.school.rumorspread.RumorSpreadComputation
